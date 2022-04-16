85f28f61516955b29c0796a33839a6260edc2d9f
FliKites <72367027+FliKites@users.noreply.github.com>
2022-04-15 21:14:24 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,23 +1,5 @@
-export const getDefaultTimeoutSeconds = () => parseInt(process.env.TIMEOUT) || 20;
-export const getConcurrency = () => parseInt(process.env.CONCURRENCY) || 33;
-export const getMaxQueueLength = () => parseInt(process.env.MAX_QUEUE_LENGTH) || 33;
+export const getDefaultTimeoutSeconds = () => parseInt(process.env.TIMEOUT) || 90;
+export const getConcurrency = () => parseInt(process.env.CONCURRENCY) || 99;
+export const getMaxQueueLength = () => parseInt(process.env.MAX_QUEUE_LENGTH) || 99;
 export const getShowResults = () => process.env.SHOW_RESULTS || 'true';
 export const getSecret = () => process.env.SECRET;
-export const browser = await puppeteer.launch({
-      headless: true,
-      ignoreHTTPSErrors: true,
-      args: [
-        "--proxy-server='http://127.0.0.1:808/'",
-        '--proxy-bypass-list=*',
-        '--disable-gpu',
-        '--disable-dev-shm-usage',
-        '--disable-setuid-sandbox',
-        '--no-first-run',
-        '--no-sandbox',
-        '--no-zygote',
-        '--single-process',
-        '--ignore-certificate-errors',
-        '--ignore-certificate-errors-spki-list',
-        '--enable-features=NetworkService'
-      ]
-    });
